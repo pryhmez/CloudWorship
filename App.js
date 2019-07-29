48abfae2cbef491a772da3d5f178594b4a1eaf01
pryhmez <edmundisaac16@gmail.com>
2019-07-29 08:45:58 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+});
+
+export default App;
